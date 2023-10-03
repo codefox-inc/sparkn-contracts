cad0076d1acb8a46cfeaec82ec88c62560c4ad9f
thurendous <thurendous2@gmail.com>
2023-10-03 13:58:17 +0900

    🔨 fix script for avalanche

--- script/DeployContracts.s.sol
+++ script/DeployContracts.s.sol
@@ -12,24 +12,44 @@ import {HelperConfig} from "./HelperConfig.s.sol";
 contract DeployContracts is Script {
     // tokens' array to whitelist
     // satadium_address = 0x5aB0ffF1a51ee78F67247ac0B90C8c1f1f54c37F
-    address[] finalTokensToWhitelist;
-    address public stadiumAddress = 0x1FBcd7D20155274DFD796343149D0FCA41338F14; // SPARKN STADDIUM
-    address public factoryAdmin = 0xbe5b0d1386BE331080fbb2C8c517BAA148497D97; // SPARKN DEV
+    address[] public finalTokensToWhitelist;
+    address public stadiumAddress = vm.envAddress("STADIUM_ADDRESS"); // SPARKN STADDIUM
+    address public factoryAdmin = vm.envAddress("SPARKN_DEV"); // SPARKN DEV
+    address public jpycv1Address;
+    address public jpycv2Address;
+    address public usdcAddress;
+    address public usdtAddress;
+    uint256 public deployerKey;
 
     function run() external returns (ProxyFactory, Distributor, HelperConfig) {
         // set up config
         HelperConfig config = new HelperConfig();
-        // get the addresses of the tokens to whitelist
-        (address jpycv1Address, address jpycv2Address, address usdcAddress,, uint256 deployerKey) =
-            config.activeNetworkConfig();
-        address[] memory tokensToWhitelist = new address[](3);
-        // whitelist 3 kinds of tokens
-        tokensToWhitelist[0] = jpycv1Address;
-        tokensToWhitelist[1] = jpycv2Address;
-        tokensToWhitelist[2] = usdcAddress;
-        for (uint256 i; i < 3; ++i) {
-            if (tokensToWhitelist[i] != address(0)) {
-                finalTokensToWhitelist.push(tokensToWhitelist[i]);
+        if (block.chainid == 43114) {
+            // get the addresses of the tokens to whitelist
+            (,jpycv2Address, usdcAddress,, deployerKey) =
+                config.activeNetworkConfig();
+            address[] memory tokensToWhitelist = new address[](2);
+            // whitelist 3 kinds of tokens
+            tokensToWhitelist[0] = jpycv2Address;
+            tokensToWhitelist[1] = usdcAddress;
+            for (uint256 i; i < tokensToWhitelist.length; ++i) {
+                if (tokensToWhitelist[i] != address(0)) {
+                    finalTokensToWhitelist.push(tokensToWhitelist[i]);
+                }
+            }
+        } else {
+            // get the addresses of the tokens to whitelist
+            (jpycv1Address, jpycv2Address, usdcAddress,, deployerKey) =
+                config.activeNetworkConfig();
+            address[] memory tokensToWhitelist = new address[](3);
+            // whitelist 3 kinds of tokens
+            tokensToWhitelist[0] = jpycv1Address;
+            tokensToWhitelist[1] = jpycv2Address;
+            tokensToWhitelist[2] = usdcAddress;
+            for (uint256 i; i < tokensToWhitelist.length; ++i) {
+                if (tokensToWhitelist[i] != address(0)) {
+                    finalTokensToWhitelist.push(tokensToWhitelist[i]);
+                }
             }
         }
 
